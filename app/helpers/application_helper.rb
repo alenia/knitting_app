5bcb2d35ac9c103faaa06dc530acb29935b04cde
Daniela Wellisz <daniela.wellisz@gmail.com>
2011-04-14 12:55:43 -0700

    Set up basic views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def title
+    base_title = "Knitting Tools"
+    return base_title if @title.nil?
+    "#{base_title} | #{@title}"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 KnittingApp::Application.routes.draw do
+  
+  match '/contact', :to => 'pages#contact'
+  
+  match '/about', :to => 'pages#about'
+  
+  match '/help', :to => 'pages#help'
+  
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
