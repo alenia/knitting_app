0cda027666899d775b464f3ec5e43749a27a00b9
Daniela Wellisz <daniela.wellisz@gmail.com>
2011-04-17 20:14:35 -0700

    show page not updating

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,9 +8,19 @@ class ProjectsController < ApplicationController
     @title = "New Project"
   end
   
+  def show
+    @project = Project.find(params[:id])
+    @title = @project.name
+  end
+  
   def index
     @title = "Projects"
     @projects = Project.all
   end
   
+  def update
+    @project = Project.find(params[:id])
+    @project.update_attributes(params[:project])
+    redirect_to projects_path
+  end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -31,4 +31,31 @@ describe ProjectsController do
       Project.where(:name => "Aidez", :current_row => 42).count.should == 1
     end
   end
+  describe "show" do
+    before do
+      @project = Project.create(:name => "Aidez", :current_row => 42)
+    end
+    it "should display the project page for a project" do
+      get :show, id: @project
+      response.should be_success
+    end
+    it "should have the right title" do
+      get :show, id: @project
+      response.should have_selector("title", :content => "Aidez")
+    end
+    it "should display the project name" do
+      get :show, id: @project
+      response.should have_selector("h1", :content => "Aidez")
+    end
+  end
+  describe "update" do
+    it "should increment when you want it to" do
+      project = Project.create(:name => "Aidez", :current_row => 42)
+      Project.where(:name => "Aidez", :current_row => 42).count.should == 1
+      Project.where(:name => "Aidez", :current_row => 43).count.should == 0
+      post :update, :project => {:current_row => (project.current_row + 1) }, id: project
+      Project.where(:name => "Aidez", :current_row => 42).count.should == 0
+      Project.where(:name => "Aidez", :current_row => 43).count.should == 1
+    end
+  end
 end
